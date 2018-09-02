71d1891afcc7a933c96f7ed6d9d26bc296be1ec5
krishnaanaril <krishnamohan.a.m@gmail.com>
2018-09-02 14:53:08 +0530

    Redesign and added about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './views/home/home.component';
 import { AboutComponent } from './views/about/about.component';
 import { ProjectsComponent } from './views/projects/projects.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 // import { MaterialModule } from './material.module';
 
 @NgModule({
@@ -21,7 +23,8 @@ import { ProjectsComponent } from './views/projects/projects.component';
   imports: [
     AppRoutingModule,
     // BrowserAnimationsModule,
-    BrowserModule,    
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),    
     // FlexLayoutModule, 
     // HttpClientModule,
     // MaterialModule
