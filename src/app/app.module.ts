ab80099d7cd71fe8b246393e4c15602694b70f50
Krishna Mohan <krishnamohan.a.m@gmail.com>
2020-04-05 08:00:58 +0530

    Remove flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { FlexLayoutModule } from "@angular/flex-layout";
-// import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
