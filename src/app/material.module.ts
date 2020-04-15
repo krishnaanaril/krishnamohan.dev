df12d9cce26fc3e295c410b97736385ced29dd8f
Krishna Mohan <krishnamohan.a.m@gmail.com>
2020-04-15 16:58:42 +0530

    add chips for displaying keywords
    this includes other style changes. Closes #5

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CdkTableModule } from '@angular/cdk/table';
+import { MatChipsModule } from '@angular/material/chips';
 import {
   MatToolbarModule
 } from '@angular/material/toolbar';
@@ -7,7 +8,8 @@ import {
 @NgModule({
   exports: [
     CdkTableModule,    
-    MatToolbarModule    
+    MatToolbarModule,
+    MatChipsModule    
   ],
 })
 export class MaterialModule {}
