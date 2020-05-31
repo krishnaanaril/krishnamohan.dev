43dd4f9ccafbda480b4785331d2c547bf2c68b93
Krishna Mohan <krishnamohan.a.m@gmail.com>
2020-05-31 16:03:29 +0530

    feat: update webp.js with *.jpeg format

--- webp.js
+++ webp.js
@@ -1,7 +1,7 @@
 const imagemin = require('imagemin'),
     webp = require("imagemin-webp"),   // imagemin's WebP plugin.
     outputFolder = "./src/assets/images",            // Output folder
-    images = './src/assets/images/*.{jpg,png}';        // PNG & JPEG images
+    images = './src/assets/images/*.{jpeg,jpg,png}';        // PNG & JPEG images
 
 
 
