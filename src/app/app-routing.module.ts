26e3914b056f85eb8b816e2cdd8bb921adfc01b6
Krishna Mohan <krishnamohan.a.m@gmail.com>
2020-06-03 17:02:57 +0530

    add extraoptions for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+    urlUpdateStrategy: 'eager'
+  })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
