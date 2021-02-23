1303961fed70e9d142e3b2058a5a1336b1da67b0
Krishna Mohan <krishnamohan.a.m@gmail.com>
2021-02-23 10:13:59 +0530

    refactor: split complex logic in blog component

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -7,9 +7,10 @@ import {
 } from '@angular/core';
 import { ActivatedRoute, Router, ROUTES } from '@angular/router';
 import { ScullyRoutesService, ScullyRoute } from '@scullyio/ng-lib';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, pipe, ReplaySubject } from 'rxjs';
 import { MetaService } from '../meta.service';
 import { MetaData } from '../meta-data.model';
+import { takeUntil } from 'rxjs/operators';
 
 declare var ng: any;
 
@@ -20,40 +21,54 @@ declare var ng: any;
   preserveWhitespaces: true,
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class BlogComponent implements OnInit, AfterViewChecked, OnDestroy {
-  private subscriptions = new Subscription();
+export class BlogComponent implements OnInit, OnDestroy {
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
   current$: Observable<any> = this.scully.getCurrent();
-  metaData: MetaData;
-
-  ngOnInit() {
-    this.subscriptions.add(
-      this.current$.subscribe((blog: ScullyRoute) => {
-        console.log(blog);
-        this.metaData = {
-          title: blog.title,
-          description: blog.description,
-          date: blog.date,
-          category: blog.category ? blog.category : '',
-          imageUrl: blog.image ? blog.image : '',
-          keywords: blog.keywords
-            ? blog.keywords.split(',').map((elem) => elem.trim())
-            : [],
-          siteUrl: blog.route,
-          type: 'website',
-        };
-        this.metaService.setMetaForCurrentPage(this.metaData);
-      })
-    );
-  }
 
   constructor(
     private scully: ScullyRoutesService,
     private metaService: MetaService
-  ) {}
+  ) { }
 
-  ngAfterViewChecked() {}
+  ngOnInit() {
+    this.current$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(
+        (blog: ScullyRoute) => this.setMetaDataInPage(blog)
+      );
+  }
+
+  /**
+   * Set metadata tags in page's html
+   * @param blog - ScullyRoute object
+   */
+  setMetaDataInPage(blog: ScullyRoute): void {
+    const metaData: MetaData = this.getMetaDataFromBlogRoute(blog);
+    this.metaService.setMetaForCurrentPage(metaData);
+  }
+
+  /**
+   * Returns MetaData object mapping from ScullyRoute
+   * @param blog - ScullyRoute object
+   */
+  getMetaDataFromBlogRoute(blog: ScullyRoute): MetaData {
+    const metaData: MetaData = {
+      title: blog.title,
+      description: blog.description,
+      date: blog.date,
+      category: blog.category ? blog.category : '',
+      imageUrl: blog.image ? blog.image : '',
+      keywords: blog.keywords
+        ? blog.keywords.split(',').map((elem) => elem.trim())
+        : [],
+      siteUrl: blog.route,
+      type: 'website',
+    };
+    return metaData;
+  }
 
   ngOnDestroy() {
-    this.subscriptions.unsubscribe();
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 }
